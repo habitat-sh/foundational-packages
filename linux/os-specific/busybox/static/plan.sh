df161f17fe3249dc759b6942f4540a259c44d57e
Ravi Duddela <rduddela@progress.com>
2025-03-10 14:40:42 +0000

    add os tools packages
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

---
+++ linux/os-specific/busybox/static/plan.sh
@@ -0,0 +1,45 @@
+pkg_name="busybox-static"
+pkg_origin="core"
+pkg_version="1.36.1"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=('GPL-2.0-only' 'bzip2-1.0.6')
+pkg_source="https://busybox.net/downloads/busybox-${pkg_version}.tar.bz2"
+pkg_shasum="b8cc24c9574d809e7279c3be349795c5d5ceb6fdf19ca709f80cde50e47de314"
+pkg_dirname="busybox-${pkg_version}"
+pkg_bin_dirs=(bin)
+pkg_interpreters=(
+	bin/ash
+	bin/awk
+	bin/env
+	bin/sh
+	bin/bash
+)
+
+pkg_build_deps=(
+	core/bzip2
+	core/gcc
+)
+
+do_prepare() {
+	export LDFLAGS="--static"
+
+	build_line "Setting LDFLAGS=${LDFLAGS}"
+}
+
+do_build() {
+	make defconfig
+	make -j"$(nproc)"
+}
+
+do_install() {
+	install -Dm755 busybox "$pkg_prefix/bin/busybox"
+
+	# Generate the symlinks back to the `busybox` executable
+	for l in $(busybox --list); do
+		ln -sv busybox "$pkg_prefix/bin/$l"
+	done
+
+	# copy license files in package
+	install -Dm644 ${CACHE_PATH}/LICENSE ${pkg_prefix}
+	#install -Dm644 ${CACHE_PATH}/archival/libarchive/bz/LICENSE ${pkg_prefix}
+}
---
+++ linux/os-specific/libcap/plan.sh
@@ -0,0 +1,41 @@
+program="libcap"
+
+pkg_name="libcap"
+pkg_origin="core"
+pkg_version="2.66"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_description="\
+The Libcap package implements the user-space interfaces to the POSIX 1003.1e capabilities \
+available in Linux kernels. These capabilities are a partitioning of the all powerful \
+root privilege into a set of distinct privileges.
+"
+pkg_upstream_url="http://sites.google.com/site/fullycapable/"
+pkg_license=('BSD-3-Clause OR GPL-2.0-only')
+pkg_source="https://git.kernel.org/pub/scm/libs/libcap/libcap.git/snapshot/${program}-${pkg_version}.tar.gz"
+pkg_shasum="20fbc13a2443881bf13f67eb4ec7f8d6b93843bf1ce7b3015ae1890ddfbd7324"
+pkg_dirname="${program}-${pkg_version}"
+
+pkg_deps=(
+	core/glibc
+)
+pkg_build_deps=(
+	core/gcc
+	core/build-tools-perl
+)
+
+pkg_bin_dirs=(bin)
+pkg_include_dirs=(include)
+pkg_lib_dirs=(lib)
+pkg_pconfig_dirs=(lib/pkgconfig)
+
+do_build() {
+	make prefix="$pkg_prefix" lib=lib sbin=bin
+}
+
+do_check() {
+	make test
+}
+
+do_install() {
+	make prefix="$pkg_prefix" lib=lib sbin=bin install
+}
