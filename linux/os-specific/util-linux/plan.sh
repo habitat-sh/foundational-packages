e456d3146c550ab68dc7fcaff40de7c94f8e3c24
Ravi Duddela <rduddela@progress.com>
2025-06-05 10:29:15 +0000

    CHEF-22556 upgrade util-linux to v2.40.4
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- linux/os-specific/util-linux/plan.sh
+++ linux/os-specific/util-linux/plan.sh
@@ -1,19 +1,20 @@
 pkg_name="util-linux"
 pkg_origin="core"
-pkg_version="2.38.1"
+pkg_version="2.40.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="Miscellaneous system utilities for Linux"
 pkg_upstream_url="https://www.kernel.org/pub/linux/utils/util-linux"
 # https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/README.licensing
 pkg_license=("GPL-2.0-only" "GPL-2.0-or-later" "LGPL-2.1-or-later" "GPL-3.0-or-later" "BSD-2-Clause" "BSD-3-Clause" "BSD-4-Clause-UC")
 pkg_source="https://www.kernel.org/pub/linux/utils/${pkg_name}/v${pkg_version%.?}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="60492a19b44e6cf9a3ddff68325b333b8b52b6c59ce3ebd6a0ecaa4c5117e84f"
+pkg_shasum="5c1daf733b04e9859afdc3bd87cc481180ee0f88b5c0946b16fdec931975fb79"
 pkg_deps=(
 	core/glibc
 	core/zlib
 	core/ncurses
 	core/readline
 	core/file
+	core/sqlite
 )
 pkg_build_deps=(
 	core/bison
