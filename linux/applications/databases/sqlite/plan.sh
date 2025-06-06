0ee715bdeb2f887915468746d339ceb45285e6d4
Ravi Duddela <rduddela@progress.com>
2025-06-06 10:42:17 +0000

    CHEF-22592 upgrade sqlite to v3.50.0
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- linux/applications/databases/sqlite/plan.sh
+++ linux/applications/databases/sqlite/plan.sh
@@ -1,15 +1,15 @@
 pkg_name="sqlite"
-pkg_version="3.45.3"
-pkg_dist_version="3450300"
+pkg_version="3.50.0"
+pkg_dist_version="3500000"
 pkg_origin="core"
-pkg_license=('LicenseRef-Public-Domain')
+pkg_license=('LicenseRef-Public-Domain' 'BSD-2-Clause-Views')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="A software library that implements a self-contained, serverless, zero-configuration, transactional SQL database engine."
 pkg_upstream_url=https://www.sqlite.org/
-pkg_source="https://www.sqlite.org/2024/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
+pkg_source="https://www.sqlite.org/2025/${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_filename="${pkg_name}-autoconf-${pkg_dist_version}.tar.gz"
 pkg_dirname="${pkg_name}-autoconf-${pkg_dist_version}"
-pkg_shasum="b2809ca53124c19c60f42bf627736eae011afdcc205bb48270a5ee9a38191531"
+pkg_shasum="3bc776a5f243897415f3b80fb74db3236501d45194c75c7f69012e4ec0128327"
 pkg_deps=(
 	core/glibc
 	core/readline
