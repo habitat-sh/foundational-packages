e609eedcd3d06bfad6bc6df329da1773d809e7d6
Ravi Duddela <rduddela@progress.com>
2025-06-10 05:55:33 +0000

    CHEF-22591 upgrade nghttp2 to 1.65.0
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- linux/development/libraries/nghttp2/plan.sh
+++ linux/development/libraries/nghttp2/plan.sh
@@ -2,13 +2,13 @@ program="nghttp2"
 
 pkg_name="nghttp2"
 pkg_origin="core"
-pkg_version="1.51.0"
+pkg_version="1.65.0"
 pkg_description="nghttp2 is an open source HTTP/2 C Library."
 pkg_upstream_url="https://nghttp2.org/"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('MIT')
 pkg_source="https://github.com/${program}/${program}/releases/download/v${pkg_version}/${program}-${pkg_version}.tar.gz"
-pkg_shasum="2a0bef286f65b35c24250432e7ec042441a8157a5b93519412d9055169d9ce54"
+pkg_shasum="8ca4f2a77ba7aac20aca3e3517a2c96cfcf7c6b064ab7d4a0809e7e4e9eb9914"
 pkg_dirname="${program}-${pkg_version}"
 pkg_deps=(
 	core/glibc
