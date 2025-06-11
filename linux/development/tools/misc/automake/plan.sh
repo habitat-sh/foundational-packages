7fc216e56466d2764650311e6d37de68b0d655ce
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2025-06-11 12:26:11 +0530

    automake bumped to 1.17 [CHEF-22423]
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- linux/development/tools/misc/automake/plan.sh
+++ linux/development/tools/misc/automake/plan.sh
@@ -1,7 +1,7 @@
 program="automake"
 pkg_name="automake"
 pkg_origin="core"
-pkg_version="1.16.5"
+pkg_version="1.17"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 Automake is a tool for automatically generating Makefile.in files compliant \
@@ -10,7 +10,7 @@ with the GNU Coding Standards.\
 pkg_upstream_url="https://www.gnu.org/software/automake/"
 pkg_license=('GPL-2.0-or-later' 'LGPL-2.1-or-later')
 pkg_source="http://ftp.gnu.org/gnu/${pkg_name}/${pkg_name}-${pkg_version}.tar.xz"
-pkg_shasum="f01d58cd6d9d77fbdca9eb4bbd5ead1988228fdb73d6f7a201f5f8d6b118b469"
+pkg_shasum="8920c1fc411e13b90bf704ef9db6f29d540e76d232cb3b2c9f4dc4cc599bd990"
 
 pkg_deps=(
 	core/perl
