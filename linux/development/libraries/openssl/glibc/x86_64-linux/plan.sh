fff783cd5219f7cd121497772bbe17457cf55d0f
Ravi Duddela <rduddela@progress.com>
2025-06-17 08:26:38 +0000

    CHEF-22849 upgrade openssl to 3.5.0
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- linux/development/libraries/openssl/glibc/x86_64-linux/plan.sh
+++ linux/development/libraries/openssl/glibc/x86_64-linux/plan.sh
@@ -2,7 +2,7 @@ program="openssl"
 
 pkg_name="openssl"
 pkg_origin="core"
-pkg_version="3.2.4"
+pkg_version="3.5.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 OpenSSL is an open source project that provides a robust, commercial-grade, \
@@ -13,7 +13,7 @@ library.\
 pkg_upstream_url="https://www.openssl.org"
 pkg_license=('Apache-2.0')
 pkg_source="https://www.openssl.org/source/${program}-${pkg_version}.tar.gz"
-pkg_shasum="b23ad7fd9f73e43ad1767e636040e88ba7c9e5775bfa5618436a0dd2c17c3716"
+pkg_shasum="344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0"
 pkg_dirname="${program}-${pkg_version}"
 pkg_deps=(
 	core/glibc
