35782188373f238ce7d349e7233a29c117bd4df7
Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>
2025-07-16 05:46:42 +0000

    Node bumped to 22.17.0 for Windows
    Signed-off-by: Sangameshwar Mandakanalli <sangameshwar.mandakanalli@progress.com>

--- windows/development/interpreters/node/plan.ps1
+++ windows/development/interpreters/node/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name="node"
 $pkg_origin="core"
-$pkg_version="22.16.0"
+$pkg_version="22.17.0"
 $pkg_description="Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine."
 $pkg_upstream_url="https://nodejs.org/"
 $pkg_license=@("MIT")
 $pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 $pkg_source="https://nodejs.org/dist/v$pkg_version/node-v$pkg_version-x64.msi"
-$pkg_shasum="e2f2802202513e1bf41f7c00307635f6c6fe31c0275c1e03d269d45a76e5fc2e"
+$pkg_shasum="896ff4a1aff30468e93e77a8884bb1aec466df7febf08daaa1d22dd5e4b14756"
 $pkg_build_deps=@("core/lessmsi")
 $pkg_bin_dirs=@("bin")
 
