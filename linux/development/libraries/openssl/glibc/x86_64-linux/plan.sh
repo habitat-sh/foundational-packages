d87cc718e6639dc6af743286ec785213ba23abbe
John McCrae <john.mccrae@progress.com>
2026-01-26 14:15:10 -0800

    Updating the plan file for Openssl 3.5.4
    Signed-off-by: John McCrae <john.mccrae@progress.com>

--- linux/development/libraries/openssl/glibc/x86_64-linux/plan.sh
+++ linux/development/libraries/openssl/glibc/x86_64-linux/plan.sh
@@ -2,7 +2,7 @@ program="openssl"
 
 pkg_name="openssl"
 pkg_origin="core"
-pkg_version="3.5.0"
+pkg_version="3.5.4"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_description="\
 OpenSSL is an open source project that provides a robust, commercial-grade, \
@@ -13,7 +13,7 @@ library.\
 pkg_upstream_url="https://www.openssl.org"
 pkg_license=('Apache-2.0')
 pkg_source="https://www.openssl.org/source/${program}-${pkg_version}.tar.gz"
-pkg_shasum="344d0a79f1a9b08029b0744e2cc401a43f9c90acd1044d09a530b4885a8e9fc0"
+pkg_shasum="967311f84955316969bdb1d8d4b983718ef42338639c621ec4c34fddef355e99"
 pkg_dirname="${program}-${pkg_version}"
 pkg_deps=(
 	core/glibc
