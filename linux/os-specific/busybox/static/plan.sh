88041e47957e4fea272ae7daa33170ea28cf4de6
Nimit <nimit.jyotiana@hotmail.com>
2026-04-14 13:09:35 +0000

    [busybox-static] upgraded to v1.37.0
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- linux/os-specific/busybox/static/plan.sh
+++ linux/os-specific/busybox/static/plan.sh
@@ -1,10 +1,10 @@
 pkg_name="busybox-static"
 pkg_origin="core"
-pkg_version="1.36.1"
+pkg_version="1.37.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=('GPL-2.0-only' 'bzip2-1.0.6')
 pkg_source="https://busybox.net/downloads/busybox-${pkg_version}.tar.bz2"
-pkg_shasum="b8cc24c9574d809e7279c3be349795c5d5ceb6fdf19ca709f80cde50e47de314"
+pkg_shasum="3311dff32e746499f4df0d5df04d7eb396382d7e108bb9250e7b519b837043a4"
 pkg_dirname="busybox-${pkg_version}"
 pkg_bin_dirs=(bin)
 pkg_interpreters=(
