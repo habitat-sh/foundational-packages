492b760357a334569c7deaf3e22e37251d5fbc51
Ravi Duddela <rduddela@progress.com>
2026-04-17 07:41:03 +0000

    CHEF-33517 windows-update docker to 29.4.0
    Signed-off-by: Ravi Duddela <rduddela@progress.com>

--- windows/applications/virtualization/docker/plan.ps1
+++ windows/applications/virtualization/docker/plan.ps1
@@ -1,12 +1,12 @@
 $pkg_name = "docker"
 $pkg_description = "The Docker Engine"
 $pkg_origin = "core"
-$pkg_version = "29.3.1"
+$pkg_version = "29.4.0"
 $pkg_maintainer = "The Habitat Maintainers <humans@habitat.sh>"
 $pkg_license = @("Apache-2.0")
 $pkg_source = "https://download.docker.com/win/static/stable/x86_64/$pkg_name-$pkg_version.zip"
 $pkg_upstream_url = "https://docs.docker.com/engine/installation/binaries/"
-$pkg_shasum = "9de0daaed958062d2005e2766c856a6671dd95e6202e1ac801c569e22dff7edf"
+$pkg_shasum = "a788f9ff37af446419a24eaf579da5c3da59995c708e6a5c908fe21519845b01"
 $pkg_dirname = "docker"
 $pkg_bin_dirs = @("bin")
 
